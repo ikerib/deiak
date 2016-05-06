dfe6b43aef9d7cd35e7d455e00cbb7cf4c12fdd0
Iker Ibarguren <ikerib@gmail.com>
2016-05-06 14:32:51 +0200

    Login controller OK

--- src/AppBundle/Controller/LoginController.php
+++ src/AppBundle/Controller/LoginController.php
@@ -33,7 +33,6 @@ class LoginController extends Controller
         ));
     }
 
-
     /**
      * Lists all Teknikoa entities.
      *
@@ -49,59 +48,12 @@ class LoginController extends Controller
         if (!$usuario) {
             throw $this->createNotFoundException('Unable to find $usuario entity.');
         }
-//        if ($usuario->getAdmin() == 1) {
             $token = new UsernamePasswordToken($usuario, null, 'main', array('ROLE_ADMIN'));
             $this->get('security.token_storage')->setToken($token);
             $this->get('session')->set('_security_main', serialize($token));
             return $this->redirect($this->generateUrl('deia_index'));
-//        } else {
-//            $token = new UsernamePasswordToken($usuario, null, 'main', array('ROLE_USER'));
-//            $this->get('security.context')->setToken($token);
-//            $this->get('session')->set('_security_main', serialize($token));
-//            return $this->redirect($this->generateUrl('menu'));
-//        }
     }
 
-
-//    /**
-//     * Lists all Teknikoa entities.
-//     *
-//     * @Route("/dologin", name="teknikoa_dologin")
-//     * @Method("POST")
-//     */
-//    public function dologinAction(Request $request)
-//    {
-//        $id = $request->request->get('username');
-//        $logger->info("888====>$id========================================");
-//        $logger->info($id);
-//        $logger->info('777============================================');
-//        $em = $this->getDoctrine()->getManager();
-//        $user = $em->getRepository('AppBundle:Teknikoa')->findOneById($id);
-//        $logger = $this->get('logger');
-//        $logger->info('666============================================');
-//        $logger->info($user);
-//        $logger->info('555============================================');
-//
-//        dump($user);
-//        if (!$user) {
-//            throw new UsernameNotFoundException("User not found");
-//        } else {
-//
-////            $token = new UsernamePasswordToken($user, null, "secured_area", $user->getRoles());
-////            $this->get('security.token_storage')->setToken($token);
-////            $this->get('session')->set('_security_main', serialize($token));
-////
-////            $event = new InteractiveLoginEvent($request, $token);
-////            $this->get("event_dispatcher")->dispatch("security.interactive_login", $event);
-//
-////            return $this->redirect($this->generateUrl("homepage"));
-//        }
-//
-////        $teknikoas = $em->getRepository('AppBundle:Teknikoa')->findAll();
-////        return $this->render('teknikoa/login.html.twig', array(
-////            'teknikoas' => $teknikoas,
-////        ));
-//    }
 
     /**
      * @Route("/logout", name="logout")
@@ -123,9 +75,6 @@ class LoginController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         $teknikoas = $em->getRepository('AppBundle:Teknikoa')->findAll();
-//        $request->getSession()->setFlash('error', $exception->getMessage());
-//        $error = $exception->getMessage();
-        dump($request);
         return $this->render('teknikoa/login.html.twig', array(
             'teknikoas' => $teknikoas,
         ));
