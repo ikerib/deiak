4bb57be761c15899caff62595b42e3f9a787590d
Iker Ibarguren <ikerib@gmail.com>
2017-08-02 14:18:47 +0200

    Router fix

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,12 +1,8 @@
-// src/components/NotFound/index.js
-import React, { PropTypes, Component } from 'react';
+import React, {  Component } from 'react';
 
 import './style.css';
 
 export default class NotFound extends Component {
-    // static propTypes = {}
-    // static defaultProps = {}
-    // state = {}
 
     render() {
         const { className, ...props } = this.props;
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,16 @@
 // src/routes.js
 import React from 'react';
-import { Route,Link } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import App from './components/App';
 import NotFound from './components/NotFound';
 
 const Routes = (props) => (
     <div>
-        <Route path="/" component={App} />
-        <Route path="*" component={NotFound} />
+        <Switch>
+            <Route path="/" exact component={App} />
+            <Route path="*" component={NotFound} />
+        </Switch>
     </div>
 );
 
